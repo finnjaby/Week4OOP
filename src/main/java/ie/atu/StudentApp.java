a89d3f0672cae1faf525e0ded58ccb54d40f6b8e
Finnja <g00470359@atu.ie>
2025-10-15 19:22:01 +0100

    changed main to StudentApp, added Student class

---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class StudentApp {
+    public static void main(String[] args) {
+
+    }
+}

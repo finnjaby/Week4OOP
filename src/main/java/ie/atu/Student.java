404755f0798bd47328529a039af551a7241087bd
Finnja <g00470359@atu.ie>
2025-10-15 19:25:00 +0100

    added default constructor, getters and setters

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,33 @@
 package ie.atu;
 
 public class Student {
+    String name;
+    String email;
+    String course;
+
+    Student(){
+        name = "not set";
+        email = "not set";
+        course = "not set";
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+    void setEmail(String email){
+        this.email = email;
+    }
+    void setCourse(String course){
+        this.course = course;
+    }
+
+    String getName(){
+        return name;
+    }
+    String getEmail(){
+        return email;
+    }
+    String getCourse(){
+        return course;
+    }
 }
